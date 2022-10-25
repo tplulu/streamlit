2279e350d654f2b7909aeaef15ca2d4d4cefcdf2
TPLulu <thibault.piotrowski@gmail.com>
2022-10-25 12:21:49 +0200

    changements

--- python_streamlit.py
+++ python_streamlit.py
@@ -26,37 +26,40 @@ pa = pa.rename(columns={"CO2 emission (Tons)": "emission"})
 pa = pa.rename(columns={"First Tooltip": "Cancer"})
 pa_small = pa.query("emission< 0.20e10")
 pa_big = pa.query("emission> 0.20e10")
-fig2, ax = plt.subplots()
-ax = sb.relplot(
-    data=pa_small.sample(n=100),
-    x="Cancer", y="emission", 
-)
-st.pyplot(fig2)
+fig, ax = plt.subplots()
+fig = sb.relplot(data=pa_small.sample(n=100),x="Cancer", y="emission")
+plt.title("nuage de point pour observer les morts par cancers en fonction des emissions de CO2")
+st.pyplot(fig)
+
 corr_pa = pa.corr()
-fig2, ax = plt.subplots()
-ax = fig = sb.heatmap(
+fig1, ax = plt.subplots()
+ax = sb.heatmap(
     data=corr_pa,
 )
-st.pyplot(fig2)
+plt.title("observation de la corrélation entre plusieurs éléments")
+st.pyplot(fig1)
 fig2, ax = plt.subplots()
-ax = sb.lmplot(
+fig2 = sb.lmplot(
     data=pa_small,
     x="Cancer", y="emission",
 )
+plt.title("On observe une relation négative entre le taux de cancer et le taux de polution d'un pays")
 st.pyplot(fig2)
 cancer_moye = cancer.groupby(['Period']).mean()
 fig2, ax = plt.subplots()
-ax = sb.relplot(
+fig2 = sb.relplot(
     data=cancer_moye,
     x="Period", y="First Tooltip", kind="line",
 )
+plt.title("Progression des morts par cancer en fonction du temps")
 st.pyplot(fig2)
 co2_moye = co2_origin.groupby(['Year']).mean()
 fig2, ax = plt.subplots()
-ax = sb.relplot(
+fig2 = sb.relplot(
     data=co2_moye,
     x="Year", y="CO2 emission (Tons)", kind="line",
 )
+plt.title("Progression des émissions de CO2 en fonction du temps")
 st.pyplot(fig2)
 st.title('Boite à moustache')
 fig2, ax = plt.subplots()
@@ -64,6 +67,7 @@ ax = sb.boxplot(
     data=cancer,
     x="First Tooltip", y="Dim1",
 )
+plt.title("On observe des valeurs abérantes")
 st.pyplot(fig2)
 
 cancer2 = cancer[(cancer["First Tooltip"]<35)]
@@ -72,11 +76,19 @@ ax = sb.boxplot(
     data=cancer2,
     x="First Tooltip", y="Dim1",
 )
+plt.title("suppression des valeurs abérantes")
 st.pyplot(fig2)
-sb.catplot(
+
+fig5, ax = plt.subplots()
+fig5 = sb.catplot(
     data=cancer2,
-    x="Dim1", y="First Tooltip", kind="bar", height=10 
+    x="Dim1", 
+    y="First Tooltip", 
+    kind="bar", 
+    height=10 
 )
+plt.title("cancer par genre")
+st.pyplot(fig5)
 
 cancer_grp = cancer.groupby(['Dim1']).mean()
 cancer2["First Tooltip"].describe()
